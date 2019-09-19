112dd0429f6d666449dc43b24e22d8bc9cc75082
Rubén Salas <rubsalas@hotmail.com>
2019-09-19 01:17:51 -0600

    java-json.jar Incluido

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,33 @@
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Main {
+	
+	public static void main (String [] args) throws JSONException
+    {
+        
+    	String jsonString;
+		
+    	JSONObject obj = new JSONObject();
+    	/*put(key,value);*/
+    	obj.put("posx","350");
+    	obj.put("posy","650");
+    	
+    	//List -> Array
+    	JSONArray list = new JSONArray();
+    	list.put("cocodrilos");
+    	list.put("frutas");
+    	list.put("dk");
+    	list.put("test");
+    	
+    	obj.put("movibles",list);
+    	
+    	jsonString = obj.toString();
+    	
+    	System.out.println(obj);
+    	
+    }
 
 }
